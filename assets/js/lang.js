0c1d9c9fc6abfd5d5476c9d837837d619c9ecb9c
Mathias <delafuente.mathias.e@gmail.com>
2024-08-20 21:35:24 -0300

    new section "mini projects"

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -2,7 +2,7 @@ const languageSelectorEl = document.getElementById('language-selector');
 
 languageSelectorEl.addEventListener('change', () => {
   const lang = languageSelectorEl.value;
-  const url = `index-${lang}.html`;
+  const url = `index${lang}.html`;
   window.location.href = url;
 });
 
